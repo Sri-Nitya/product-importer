6de3f7fa6af339ae366fe2224a3db05947e04673
Nitya Narigiri <srinitya.narigiri03@gmail.com>
2025-11-15 17:48:28 +0530

    Upload via UI with progress bar

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from starlette.staticfiles import StaticFiles
 from app.api.upload import router as upload_router
 
 app = FastAPI(title="Product Importer API")
 
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
 @app.get("/")
 def root():
-    return {"message": "Welcome to the Product Importer API"}
+    return FileResponse("app/static/index.html")
 
 app.include_router(upload_router)
